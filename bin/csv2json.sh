5e35db3938f17f0977ac6cf74ed9691e57dc9e10
Guy Cole <guycole@gmail.com>
2025-11-02 09:41:45 -0800

    add daily reboot

--- bin/csv2json.sh
+++ bin/csv2json.sh
@@ -9,15 +9,11 @@ PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
 HOME_DIR="${HOME}/Documents/github"
 #
-COOKED_DIR="cooked"
-PROCESSED_DIR="processed"
-WORK_DIR="/var/mellow/mastodon"
-WORK_DIR="/mnt/sda/gsc/mellow/mastodon"
-#
 echo "begin csv2json"
+#
 cd $HOME_DIR/mellow-mastodon/src/collector 
 source venv/bin/activate
-time python3 ./csv2json.py
+time python3 csv2json.py
 # 
 echo "end csv2json"
 #
